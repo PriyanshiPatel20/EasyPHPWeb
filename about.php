74b7a01a43fba645e638676014fcf77a31e11b63
PriyanshiPatel20 <patelpriyanshi8585@gmail.com>
2024-04-03 01:38:24 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  
+        <title>Website</title>
+</head>
+<body>
+    
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Website</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>Nature</h1>
+  <p>We The Web Developers.</p>
+</div>
+
+<section class="my-5">
+    <div class="py-3">
+        <h2 class="text-center">About Us</h2>
+    </div> 
+    <div class="container-fluid">  
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12 ">  
+                    <img src="images/bird1.jpeg" class="img-fluid aboutimg" alt=""> 
+                </div>
+                <div class="col-lg-6 col-md-6 col-12 ">  
+                 <h2>I am Priyanshi Patel</h2> 
+                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores reprehenderit consectetur quae laboriosam, provident esse, accusamus voluptates aspernatur earum incidunt voluptatibus fugiat eius! Saepe, non. Minus tempora deserunt amet mollitia?</p>
+                <a href="about.php" class="btn btn-outline-success">Check More</a>
+                </div>
+            </div>
+    </div>  
+</section>
+<fotter>
+    <p class="p-3 bg-dark text-white text-center" >@priyanshipatelProduction</p>
+</fotter>
+
+</body>
+</html>
